0087a94dc83d2d2eb5f2fc936455f72c7baf2990
Alex <tangyongjin@gmail.com>
2018-05-21 20:37:11 +0800

    activiti-rest-client 挪到到 codeigniter-restserver/ 下面, Books 示范get/post完成

--- codeigniter-restserver/application/controllers/Books.php
+++ codeigniter-restserver/application/controllers/Books.php
@@ -1,10 +1,19 @@
 <?php
+
 defined('BASEPATH') OR exit('No direct script access allowed');
 // This can be removed if you use __autoload() in config.php OR use Modular Extensions
 /** @noinspection PhpIncludeInspection */
 
 
 require('application/libraries/REST_Controller.php');
+
+require(dirname(dirname(__DIR__)).'/activiti-rest-client/client/ActivitiClient.php');
+require(dirname(dirname(__DIR__)).'/activiti-rest-client/client/objects/ActivitiStartProcessInstanceRequestVariable.php');
+
+// require_once(__DIR__ . '../client/objects/ActivitiStartProcessInstanceRequestVariable.php');
+
+
+
 use Restserver\Libraries\REST_Controller;
 /**
  * This is an example of a few basic user interaction methods you could use
@@ -18,24 +27,60 @@ use Restserver\Libraries\REST_Controller;
  * @link            https://github.com/chriskacerguis/codeigniter-restserver
  */
 class Books extends \Restserver\Libraries\REST_Controller {
+
     function __construct()
     {
         // Construct the parent class
         parent::__construct();
         // Configure limits on our controller methods
         // Ensure you have created the 'limits' table and enabled 'limits' within application/config/rest.php
+     
+        header("Access-Control-Allow-Credentials: true"); 
+        // header("Access-Control-Allow-Origin: http://47.92.72.19:8090 "); 
+        header("Access-Control-Allow-Origin: * "); 
+        header("Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Accept"); 
+
+
+
+
+
+
         $this->methods['users_get']['limit'] = 500; // 500 requests per hour per user/key
         $this->methods['users_post']['limit'] = 100; // 100 requests per hour per user/key
         $this->methods['users_delete']['limit'] = 50; // 50 requests per hour per user/key
     }
+
+
+    public function callsth_get(){
+
+         $para1 = $this->get('para1');
+         $para2 = $this->get('para2');
+         $para3 = $this->get('para3');
+
+         print_r($para1);
+         print_r($para2);
+
+    }
+    
     public function users_get()
     {
+
+
+
+        header("Access-Control-Allow-Credentials: true"); 
+        // header("Access-Control-Allow-Origin: http://47.92.72.19:8090 "); 
+        header("Access-Control-Allow-Origin: * "); 
+        header("Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Accept"); 
+        $activiti = new ActivitiClient();
+ 
         // Users from a data store e.g. database
+        
         $users = [
-            ['id' => 1, 'name' => 'John', 'email' => 'john@example.com', 'fact' => 'Loves coding'],
-            ['id' => 2, 'name' => 'Jim', 'email' => 'jim@example.com', 'fact' => 'Developed on CodeIgniter'],
-            ['id' => 3, 'name' => 'Jane', 'email' => 'jane@example.com', 'fact' => 'Lives in the USA', ['hobbies' => ['guitar', 'cycling']]],
+            ['id' => 1, 'name' => 'John', 'email' => 'john@example.com', 'fact' => 'Loves'],
+            ['id' => 2, 'name' => 'Jim', 'email' => 'jim@example.com', 'fact' => 'Developed'],
+            ['id' => 3, 'name' => 'Jane', 'email' => 'jane@example.com', 'fact' => 'LivesUSA', ['hobbies' => ['guitar', 'cycling']]],
         ];
+
         $id = $this->get('id');
         // If the id parameter doesn't exist return all the users
         if ($id === NULL)
@@ -87,7 +132,14 @@ class Books extends \Restserver\Libraries\REST_Controller {
                 'message' => 'User could not be found'
             ], \Restserver\Libraries\REST_Controller::HTTP_NOT_FOUND); // NOT_FOUND (404) being the HTTP response code
         }
+    
+
+        
+
+
+
     }
+    
     public function users_post()
     {
         // $this->some_model->update_user( ... );
@@ -99,6 +151,9 @@ class Books extends \Restserver\Libraries\REST_Controller {
         ];
         $this->set_response($message, \Restserver\Libraries\REST_Controller::HTTP_CREATED); // CREATED (201) being the HTTP response code
     }
+
+
+    
     public function users_delete()
     {
         $id = (int) $this->get('id');
@@ -115,4 +170,11 @@ class Books extends \Restserver\Libraries\REST_Controller {
         ];
         $this->set_response($message, \Restserver\Libraries\REST_Controller::HTTP_NO_CONTENT); // NO_CONTENT (204) being the HTTP response code
     }
+
+
+
+
+
+
+
 }
